777e66194be412c428b2b899101e4d7515df9c66
Sindre Moldeklev <sndrem@live.no>
2017-02-25 14:06:21 +0100

    Initial Commit

---
+++ minTur/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  minTur
+//
+//  Created by Sindre Moldeklev on 25.02.2017.
+//  Copyright © 2017 Sindre Moldeklev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
